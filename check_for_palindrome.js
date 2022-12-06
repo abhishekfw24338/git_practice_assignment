8c4013939837e1be03d1988f3de5bc8b849ebaba
abhishek singh <abhisheksingh74560@gmail.com>
2022-12-06 21:01:03 +0530

    finding_prime_and_palindrome

---
+++ check_for_palindrome.js
@@ -0,0 +1,21 @@
+
+//Check whether a string is palindrome or not.
+
+let str="madam";
+let bag="";
+for(let i=str.length-1; i>=0; i--){
+	
+	bag=bag+str[i];
+
+}
+
+if(bag==str){
+
+	console.log("It is a palindrome");
+
+}
+else{
+
+	console.log("It is not a palindrome)
+
+}
---
+++ check_for_prime_number.js
@@ -0,0 +1,23 @@
+// Check whether a number is Prime or not
+
+let a=13;
+let count=0;
+for(let i=1;i<=13;i++){
+
+	if(a%i==0){
+	count++
+
+	}
+
+}
+if(count==2){
+	
+	console.log("It is a prime number")
+
+}
+
+else{
+	console.log("It is not a prime number")
+
+}
+
